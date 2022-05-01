8c8db21e0133f2e16d2cab9d3294a1e0de5431df
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2022-05-01 14:19:45 +0300

    Renamed tutorial.py and updated it

---
+++ main.py
@@ -0,0 +1,217 @@
+# import pygame and math modules
+import pygame
+import math
+
+import datetime
+
+# import some color-related modules
+import matplotlib.colors as mc
+import colorsys
+
+# initialise the pygame modules
+pygame.init()
+
+# define window properties
+width, height = 800, 800
+window = pygame.display.set_mode((width, height))
+surface = pygame.Surface((width*2, height*2))
+pygame.display.set_caption('Planet Simulation')
+
+font = pygame.font.SysFont('comicsans', 16)
+
+# define a colors class
+class colors:
+	white = (255, 255, 255)
+	yellow = (255, 255, 0)
+	blue = (100, 149, 237)
+	red = (188, 39, 50)
+	dark_grey = (80, 78, 81)
+
+
+def adjust_lightness(input_color, amount):
+	color = (input_color[0]/255, input_color[1]/255, input_color[2]/255)
+	try:
+		c = mc.cnames[color]
+	except:
+		c = color
+	c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+	float_color = colorsys.hls_to_rgb(
+		c[0], max(0, min(1, amount / 255 * c[1])), c[2])
+	return list(round(i*255) for i in float_color)
+
+# define a Planet class
+class Planet:
+	AU = 149.6e6 * 1000
+	G = 6.67428e-11
+	scale = 225 / AU  # 1AU = x pixels where x is the number next to / AU
+	current_time = 0
+	timestep = 3600*6  # 6 hours
+	display_step = 3600*24*7  # 7 days
+
+	def __init__(self, x, y, radius, color, mass):
+		self.x = x
+		self.y = y
+		self.radius = radius
+		self.color = color
+		self.mass = mass
+
+
+		self.orbit = []
+		self.is_sun = False
+		self.distance_to_sun = 0
+
+		self.x_vel = 0
+		self.y_vel = 0
+
+	def draw(self, win, surf, x_movement_factor = 0, y_movement_factor = 0):
+		x = self.x * self.scale + width / 2
+		y = self.y * self.scale + height / 2
+
+		if len(self.orbit) > 2:
+			updated_points = []
+			for point in self.orbit:
+				x, y = point
+				x = x * self.scale + width / 2
+				y = y * self.scale + height / 2
+				updated_points.append((x, y))
+
+			# draw each one of the last 255 lines inputed on the list a bit darker
+			for hue, coords in enumerate(updated_points[-255:-1]):
+				pygame.draw.line(surf, adjust_lightness(
+					self.color, hue), coords, updated_points[-255:][hue+1])
+				#pygame.draw.circle(surf, adjust_lightness(self.color, hue), coords, 1)
+			#pygame.draw.lines(surf, self.color, False, updated_points[-255:], 2)
+
+		pygame.draw.circle(surf, self.color, (x, y), self.radius)
+
+		if not self.is_sun:
+			distance_text = font.render(
+				f"{round(self.distance_to_sun/1000)} km", 1, colors.white)
+			surf.blit(distance_text, (x, y + distance_text.get_height()/2))
+	
+		win.blit(surf, (x_movement_factor, y_movement_factor))
+
+	def attraction(self, other):
+		# get distance to "other"
+		other_x, other_y = other.x, other.y
+		distance_x = other_x - self.x
+		distance_y = other_y - self.y
+		distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+
+		# if other is the Sun, save it
+		if other.is_sun:
+			self.distance_to_sun = distance
+
+		# calculate the force
+		force = self.G * self.mass * other.mass / distance**2
+		# caculate the theta angle
+		theta = math.atan2(distance_y, distance_x)
+		force_x = math.cos(theta) * force
+		force_y = math.sin(theta) * force
+		return force_x, force_y
+
+	def update_position(self, planets, x_movement_factor=0, y_movement_factor=0, insert_to_orbit=False):
+		total_fx = total_fy = 0
+		for planet in planets:
+			if self == planet:
+				continue
+
+			fx, fy = self.attraction(planet)
+			total_fx += fx
+			total_fy += fy
+
+		self.x_vel += total_fx / self.mass * self.timestep
+		self.y_vel += total_fy / self.mass * self.timestep
+
+		self.x += self.x_vel * self.timestep
+		self.y += self.y_vel * self.timestep
+		if insert_to_orbit:
+			self.orbit.append((self.x, self.y))
+
+			time_text = font.render(
+				f"{datetime.timedelta(seconds=self.current_time)}", 1, colors.white)
+			surface.blit(time_text, (width-time_text.get_width()-15 - x_movement_factor, 5 - y_movement_factor))
+
+
+def main():
+	run = True
+	clock = pygame.time.Clock()
+
+	#define variables
+	movement_factors = [0, 0]
+
+	# define planets
+	sun = Planet(0, 0, 30, colors.yellow, 1.98892 * 10**30)
+	sun.is_sun = True
+
+	earth = Planet(-1 * Planet.AU,  0, 16, colors.blue, 5.9742 * 10**24)
+	earth.y_vel = 29.783 * 1000
+
+	mars = Planet(-1.524 * Planet.AU, 0, 12, colors.red, 6.39 * 10**23)
+	mars.y_vel = 24.077 * 1000
+
+	mercury = Planet(0.387 * Planet.AU, 0, 8, colors.dark_grey, 3.30 * 10**23)
+	mercury.y_vel = 47.4 * 1000
+
+	venus = Planet(0.723 * Planet.AU, 0, 14, colors.white, 4.8685 * 10**24)
+	venus.y_vel = -35.02 * 1000
+
+	planets = [sun, earth, mars, mercury, venus]
+
+	while run:
+		#set some variables
+		global surface
+		screen_drag = False
+
+		# run at 60fps
+		clock.tick(60)
+		surface.fill((0, 0, 0))
+		window.blit(surface, (0, 0))
+
+		current_events = pygame.event.get()
+
+		for event in current_events:
+			if event.type == pygame.QUIT:
+				run = False
+			elif event.type == pygame.MOUSEWHEEL:
+				print(f"Scroll: {event.y}")
+
+				#scale the planets
+				for planet in planets:
+					planet.scale = planet.scale * (1+event.y/10)
+					planet.radius = planet.radius * (1+event.y/10)
+
+			elif event.type == pygame.MOUSEMOTION:
+				if pygame.mouse.get_pressed(num_buttons=3)[0]:
+					mouse_x, mouse_y = event.rel
+					print(f'Mouse X: {mouse_x}, Mouse Y: {mouse_y}')
+					for planet in planets:
+						planet.x += planet.AU * mouse_x / 30
+						planet.y += planet.AU * mouse_y / 30
+
+						if len(planet.orbit) < 255:
+							for index, pos in enumerate(planet.orbit[-len(planet.orbit):]):
+								planet.orbit[-len(planet.orbit)+index] = [pos[0] + planet.AU * mouse_x / 30, pos[1] + planet.AU * mouse_y / 30]
+						else:
+							for index, pos in enumerate(planet.orbit[-255:]):
+								planet.orbit[-255+index] = [pos[0] + planet.AU * mouse_x / 30, pos[1] + planet.AU * mouse_y / 30]
+
+
+		for planet in planets:
+			# draw the planets
+			for i in range(planet.display_step//planet.timestep):
+				planet.update_position(planets)
+				planet.current_time += planet.timestep
+
+			planet.update_position(planets, insert_to_orbit=True)
+			planet.current_time += planet.timestep
+
+			planet.draw(window, surface)
+
+		pygame.display.update()
+
+	pygame.quit()
+
+
+if __name__ == "__main__":
+	main()
